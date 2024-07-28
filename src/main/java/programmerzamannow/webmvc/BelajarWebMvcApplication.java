b0607f65b4eee57f923137d197115cf83dd9e976
portfolioaidiladam <portfolioaidiladam@gmail.com>
2024-07-28 19:36:58 +0700

    first commit

---
+++ src/main/java/programmerzamannow/webmvc/BelajarWebMvcApplication.java
@@ -0,0 +1,13 @@
+package programmerzamannow.webmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarWebMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarWebMvcApplication.class, args);
+	}
+
+}
