f1d112a3936355241c9e67f27e532e6d2c8f34ad
portfolioaidiladam <portfolioaidiladam@gmail.com>
2024-08-12 10:02:40 +0700

    Belajar Integration Test

--- src/main/java/programmerzamannow/webmvc/controller/HelloController.java
+++ src/main/java/programmerzamannow/webmvc/controller/HelloController.java
@@ -1,12 +1,10 @@
 package programmerzamannow.webmvc.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import programmerzamannow.webmvc.service.HelloService;
---
+++ src/test/java/programmerzamannow/webmvc/controller/HellowControllerIntegrationTest.java
@@ -0,0 +1,38 @@
+package programmerzamannow.webmvc.controller;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HellowControllerIntegrationTest {
+
+    @LocalServerPort
+    private Integer port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void helloGuest(){
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity("http://localhost:" + port + "/hello", String.class);
+        String response = responseEntity.getBody();
+
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals("Hello Guest", response.trim());
+    }
+
+    @Test
+    void helloAidil(){
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity("http://localhost:" + port + "/hello?name=Aidil", String.class);
+        String response = responseEntity.getBody();
+
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals("Hello Aidil", response.trim());
+    }
+
+}
