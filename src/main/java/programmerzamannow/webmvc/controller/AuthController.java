3bfdc31aa927e953885c27f3a71823f9019b8443
portfolioaidiladam <portfolioaidiladam@gmail.com>
2024-08-15 12:04:29 +0700

    Belajar respon status sampai json

---
+++ src/main/java/programmerzamannow/webmvc/controller/AuthController.java
@@ -0,0 +1,43 @@
+package programmerzamannow.webmvc.controller;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import programmerzamannow.webmvc.model.User;
+
+@Controller
+public class AuthController {
+
+    @PostMapping(path = "/auth/login", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public ResponseEntity<String> login(
+            @RequestParam(name = "username") String username,
+            @RequestParam(name = "password") String password,
+            //cookie
+            HttpServletRequest servletRequest,
+            HttpServletResponse servletResponse
+    ) {
+        if ("eko".equals(username) && "rahasia".equals(password)) {
+            HttpSession session = servletRequest.getSession(true);
+            session.setAttribute("user", new User(username));
+            //cookie
+            Cookie cookie = new Cookie("username", username);
+            cookie.setPath("/");
+            servletResponse.addCookie(cookie);
+            //cookie
+            return ResponseEntity.status(HttpStatus.OK).body("OK");
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("KO");
+        }
+    }
+    // untuk mendapatkan cookienya pakai anotation @CookieValue
+    @GetMapping(path = "/auth/user")
+    public ResponseEntity<String> getUser(@CookieValue("username") String username){
+        return ResponseEntity.ok("Hello " + username);
+    }
+}
